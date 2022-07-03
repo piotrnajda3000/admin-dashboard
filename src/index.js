ba31ecd88d1a2e4639c4c51b895bb1122d30ab2a
Piotr Najda <najdapiotr79@gmail.com>
2022-07-04 01:04:39 +0200

    alignment updates

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const [...avatars] = document.querySelectorAll("[data-id]");
 
 import profilePic1 from "./assets/profilePic2.jpg";
 import profilePic2 from "./assets/profilePic3.jpg";
+import profilePic3 from "./assets/profilePic4.jpg";
 
-let profilePics = [profilePic1, profilePic2];
+let profilePics = [profilePic1, profilePic2, profilePic3];
 
 let count = 0;
 avatars.forEach((avatar) => {
