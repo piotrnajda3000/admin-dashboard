91d48a8ded4f6bda72b93b1fb8187e1f8b3f2f81
Piotr Najda <najdapiotr79@gmail.com>
2022-07-08 20:36:44 +0200

    update

--- .babelrc.js
+++ .babelrc.js
@@ -1,22 +1,22 @@
-const {
-  generateScopedNameFactory,
-} = require("@dr.pogodin/babel-plugin-react-css-modules/utils");
+// const {
+//   generateScopedNameFactory,
+// } = require("@dr.pogodin/babel-plugin-react-css-modules/utils");
 
-module.exports = {
-  plugins: [
-    [
-      "@dr.pogodin/react-css-modules",
-      {
-        generateScopedName:
-          // The classname template MUST match "localIdentName" option value
-          // you passed to "css-loader".
-          generateScopedNameFactory("[name]__[local]___[hash:base64:6]"),
-        filetypes: {
-          ".scss": {
-            syntax: "postcss-scss",
-          },
-        },
-      },
-    ],
-  ],
-};
+// module.exports = {
+//   plugins: [
+//     [
+//       "@dr.pogodin/react-css-modules",
+//       {
+//         generateScopedName:
+//           // The classname template MUST match "localIdentName" option value
+//           // you passed to "css-loader".
+//           generateScopedNameFactory("[name]__[local]___[hash:base64:6]"),
+//         filetypes: {
+//           ".scss": {
+//             syntax: "postcss-scss",
+//           },
+//         },
+//       },
+//     ],
+//   ],
+// };
